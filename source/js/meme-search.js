8dcf20df6105b684aa3360a9d2e60bd0e19b1494
mengsiliulian <mengsiliulian@users.noreply.github.com>
2026-08-08 10:19:56 +0800

    安全加固: esc补单引号转义,彻底防属性注入

--- source/js/meme-search.js
+++ source/js/meme-search.js
@@ -217,7 +217,7 @@
   function esc(s) {
     return String(s == null ? '' : s)
       .replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;')
-      .replace(/"/g, '&quot;');
+      .replace(/"/g, '&quot;').replace(/'/g, '&#39;');
   }
 
   /* ---------- 事件 ---------- */
